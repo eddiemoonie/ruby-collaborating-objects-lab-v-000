4e34c0771337df7208d1355c66451b8e4743a547
Eddie Moon <eddiemoonie@gmail.com>
2018-03-08 20:47:02 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,6 +9,10 @@ class Artist
     @songs = []
   end
 
+  def self.all
+    @@all
+  end
+
   def self.find_or_create_by_name(name)
     self.find(name) ? self.find(name) : self.create_by_name(name)
   end
