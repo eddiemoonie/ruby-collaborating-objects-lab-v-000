c6c24c88ed421534c3232b28726f0de595cd0db5
Eddie Moon <eddiemoonie@gmail.com>
2018-03-08 21:02:43 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,8 +10,8 @@ class MP3Importer
     @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
   end
 
-  def import(file_name)
-    files.each{|f| Song.new_by_filename(f)}
+  def import
+    files.each{|file_name| Song.new_by_filename(file_name)}
   end
 
 end
